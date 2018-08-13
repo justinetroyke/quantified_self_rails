5e01c9c33cd997e6099870d6dd04310c9df31f96
Justine <jjtroyke@gmail.com>
2018-08-13 15:59:25 -0600

    Created tests for returing a specific food or a 404 error

--- spec/requests/api/v1/foods/foods_request_spec.rb
+++ spec/requests/api/v1/foods/foods_request_spec.rb
@@ -12,4 +12,21 @@ describe "Foods API" do
 
     expect(foods.count).to eq(3)
   end
+
+  it "sends a specific food" do
+    id = create(:food).id
+
+    get "/api/v1/foods/#{id}"
+
+    food = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(food["id"]).to eq(id)
+  end
+
+  it "returns 404 if a specific food is not found" do
+    get "/api/v1/foods/10"
+
+    expect(status).to eq(404)
+  end
 end
