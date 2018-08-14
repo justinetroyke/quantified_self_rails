873ed2d687f861ca15de7c4c4907a9741b50f9d8
Justine <jjtroyke@gmail.com>
2018-08-14 17:37:44 -0600

    Create food completed

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -9,4 +9,15 @@ class Api::V1::FoodsController < ApplicationController
   rescue
       render status: 404, json: {}
   end
+
+  def create
+    food = Food.new(food_params)
+    food.save
+    render json: food
+  end
+
+private
+  def food_params
+    params.require(:food).permit(:name, :calories)
+  end
 end
--- spec/requests/api/v1/foods/foods_request_spec.rb
+++ spec/requests/api/v1/foods/foods_request_spec.rb
@@ -29,4 +29,27 @@ describe "Foods API" do
 
     expect(status).to eq(404)
   end
+
+  it "creates a food" do
+    params = { 'food': { 'name:': 'Donut', 'calories': '1000'} }
+    params2 = { 'food': { 'name:': 'Pizza', 'calories': '1000'} }
+    params3 = { 'food': { 'name:': 'Apple', 'calories': '12'} }
+
+    post "/api/v1/foods", params: params
+
+    expect(response).to be_successful
+
+    expect(Food.count).to eq(1)
+
+    post "/api/v1/foods", params: params2
+    post "/api/v1/foods", params: params3
+
+    expect(Food.count).to eq(3)
+  end
+
+  it "returns 404 if a specific food is not found" do
+    get "/api/v1/foods/10"
+
+    expect(status).to eq(404)
+  end
 end
