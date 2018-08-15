fbe823919ebffcdf4b55418e2ba67fc93cb34922
Justine <jjtroyke@gmail.com>
2018-08-15 13:53:30 -0600

    added config api

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,19 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module QuantifiedSelfRails
   class Application < Rails::Application
+    config.api_only = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options, :patch,
-          :delete, :create]
+        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :delete, :create]
       end
     end
 
